a162551b9c61202537add4f20d4d482e07941cc4
rabbit0084 <yumihayakawa0202@gmail.com>
2026-08-28 21:40:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-const CACHE = 'english-words-v4';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// Service worker intentionally kept minimal to avoid stale iPad cache.
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
